3c262589a97714d29833907b2c62dd7e772b8c36
aletambo <alessiotamborini@gmail.com>
2020-02-05 21:27:33 -0800

    MACD new file

---
+++ Trading.py
@@ -0,0 +1,67 @@
+from matplotlib import pyplot as plt
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+
+ALPHA_VANTAGE_API = '8419TBA0B4AAB8NS'
+ts = TimeSeries(key = ALPHA_VANTAGE_API, output_format = 'pandas')
+ti = TechIndicators(key = ALPHA_VANTAGE_API, output_format = 'pandas')
+
+ticker = 'TXMD'
+
+#get data from the stock
+data, meta = ts.get_daily(symbol=ticker, outputsize = 'full')
+data_MACD, _ = ti.get_macd(symbol = ticker, interval = 'daily', 
+	series_type = 'close', fastperiod = 26, slowperiod = 52, signalperiod = 12)
+data_SMA, _ = ti.get_sma(symbol = ticker, interval = 'daily', series_type = 'close', time_period = '10')
+
+
+daily_values = data['4. close'].values
+daily_volumes = data['5. volume'].values
+daily_sma = data_SMA['SMA'].values #NOTE: daily SMA is in reverse (earliest to latest)
+days = data.index
+
+daily_values_percent = [(daily_values[i]-daily_values[i+1])/daily_values[i+1] for i in range(len(daily_values)-1)]
+daily_volumes_percent = [ (daily_volumes[i]-daily_volumes[i+1])/(daily_volumes[i+1]) for i in range(len(daily_volumes) - 1)]
+
+macd_signal = data_MACD['MACD_Signal'].values
+macd = data_MACD['MACD'].values
+
+
+####
+period = 90
+fig, ax1 = plt.subplots()
+ax1.set_xlabel('time (days)')
+ax1.set_ylabel('price ($)', color = 'red')
+ax1.plot(days[:period], daily_values[:period], color = 'red')
+ax1.tick_params(axis = 'y', labelcolor = 'red')
+
+ax2 = ax1.twinx()
+ax2.set_ylabel('MACD', color = 'blue')
+# ax2.plot(days[:period], daily_volumes_percent[:period], color = 'blue')
+ax2.plot(days[:period], macd_signal[:period], color = 'blue', label = 'Signal')
+ax2.plot(days[:period], macd[:period], color = 'green', label = 'MACD')
+ax2.tick_params(axis = 'y', labelcolor = 'blue')
+
+fig.tight_layout()
+plt.suptitle(ticker + ' price and MACD Signal')
+plt.legend()
+plt.show()
+
+fig, ax1 = plt.subplots()
+ax1.set_xlabel('time (days)')
+ax1.set_ylabel('price ($)', color = 'red')
+ax1.plot(days[:period], daily_values[:period], color = 'red')
+ax1.tick_params(axis = 'y', labelcolor = 'red')
+
+ax2 = ax1.twinx()
+ax2.set_ylabel('Volume change (%)', color = 'blue')
+ax2.plot(days[:period], daily_volumes[:period], color = 'blue')
+# ax2.plot(days[:period], daily_values_percent[:period], color = 'blue', label = 'Signal')
+ax2.tick_params(axis = 'y', labelcolor = 'blue')
+
+fig.tight_layout()
+plt.suptitle(ticker + ' price + volume %')
+plt.legend()
+plt.show()
+
+print('Closing Price: ', round(daily_values[0],2))
